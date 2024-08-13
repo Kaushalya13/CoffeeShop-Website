1506cc22e3d3746e7fbbada55057d6f3b0def00d
username <usermail>
2024-08-13 16:47:41 +0530

    Done Menu page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from "./components/Navbar";
 import Home from './components/Home';
+import Menu from './components/Menu';
 
 const App = () => {
   return (
@@ -10,6 +11,10 @@ const App = () => {
       <main>
         <div id='home'>
           <Home />
+        </div>
+
+        <div id='menu'>
+          <Menu />
         </div>
       </main>
     </div>
